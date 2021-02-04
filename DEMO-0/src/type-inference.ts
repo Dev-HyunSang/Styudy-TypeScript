d57f61d58baff43f65eddd196dc0442b1e728fc6
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-02-04 21:23:59 +0900

    [UPDATE🛠] Type Inference

---
+++ DEMO-0/src/type-inference.ts
@@ -0,0 +1,10 @@
+// eslient-disable 
+
+let inferredCityName = "London";
+inferredCityName = "Liverpool";
+
+let inferredcityPopulation = 8000000;
+inferredcityPopulation = 4000000;
+
+let inferredisCapital = true;
+inferredisCapital = false;
