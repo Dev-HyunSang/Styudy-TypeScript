588a44d6353dc6491eb298973ced842bf0619ee4
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-02-04 20:08:39 +0900

    [Build]

---
+++ DEMO-0/build/unio-types.js
@@ -0,0 +1,13 @@
+// eslient-disable 
+var unionCityPopulation = 8000000;
+unionCityPopulation = "HyunSang Park";
+unionCityPopulation = true;
+if (typeof unionCityPopulation === 'string') {
+    console.log("String", unionCityPopulation);
+}
+else if (typeof unionCityPopulation === 'number') {
+    console.log("Number", unionCityPopulation);
+}
+else if (typeof unionCityPopulation === 'boolean') {
+    console.log("Boolean", unionCityPopulation);
+}
